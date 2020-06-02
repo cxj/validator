2732d15aaa661ca2c8d594ab5550b3b9e628d3c0
Chris Johnson <cxjohnson@gmail.com>
2020-06-01 19:35:22 -0500

    First working PHPUnit test.

--- tests/ValidatorTest.php
+++ tests/ValidatorTest.php
@@ -6,8 +6,23 @@
 
 use Cxj\Validator\Validator;
 use PHPUnit\Framework\TestCase;
+use Cxj\Validator\Success;
 
 class ValidatorTest extends TestCase
 {
+    protected Validator $validator;
 
+    public function setUp(): void
+    {
+        $this->validator = new Validator();
+    }
+
+    public function testStringOk(): void
+    {
+        $v = $this->validator->create("string", "Is a string");
+        $result = $v(Success::of("123"));
+
+        $this->assertInstanceOf(Success::class, $result);
+        $this->assertEquals("123", $result->value());
+    }
 }
